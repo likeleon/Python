e261f9c86440fe4374091061488d82acb649e263
Sung Ho Kim <likeleon@gmail.com>
2016-04-12 01:05:59 +0900

    7

---
+++ ex7.py
@@ -0,0 +1,14 @@
+print("마리에겐 꼬마 양이 있지.")
+print("양털은 %s처럼 새하얗네." % '눈')
+print("그리고 마리가 가는 곳마다.")
+print("." * 10)
+
+end1 = "맛"
+end2 = "있"
+end3 = "는"
+end4 = "불"
+end5 = "고"
+end6 = "기"
+
+print(end1 + end2 + end3, end=' ')
+print(end4 + end5 + end6)
