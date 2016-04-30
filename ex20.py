5b65d6c7151dbeb6558b326783b1479aac2bd22e
Sung Ho Kim <likeleon@gmail.com>
2016-04-30 23:57:09 +0900

    18 20

---
+++ ex20.py
@@ -0,0 +1,30 @@
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+    print(f.read())
+
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
+
+current_file = open(input_file)
+
+print("파일 전체 출력\n")
+print_all(current_file)
+
+print("되감기.")
+rewind(current_file)
+
+print("세 줄을 출력.")
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
