1c3eae3d2e0b70065ef42f87cf0966b2a6dde0fc
Sung Ho Kim <likeleon@gmail.com>
2016-05-13 01:37:14 +0900

    cb

---
+++ cookbook/8.1 인스턴스의 문자열 표현식 변형.py
@@ -0,0 +1,16 @@
+class Pair:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return 'Pair({0.x!r}, {0.y!r})'.format(self)
+    def __str__(self):
+        return '({0.x!r}, {0.y!r})'.format(self)
+
+p = Pair(3, 4)
+print(p)
+print('p is {0!r}'.format(p))
+print('p is {0}'.format(p))
+
+x = eval(repr(p))
+print(x)
