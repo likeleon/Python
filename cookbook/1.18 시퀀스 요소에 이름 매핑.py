ccb77168793b1c35235230ef52bf83d6e9b77538
Sung Ho Kim <likeleon@gmail.com>
2016-05-23 01:42:32 +0900

    cb

--- cookbook/1.18 시퀀스 요소에 이름 매핑.py
+++ cookbook/1.18 시퀀스 요소에 이름 매핑.py
@@ -24,3 +24,25 @@ def compute_cost(records):
         s = Stock(*rec)
         total += s.shares * s.price
     return total
+
+s = Stock('ACME', 100, 123.45)
+print(s)
+
+s = s._replace(shares=75)
+print(s)
+
+
+#
+from collections import namedtuple
+
+Stock = namedtuple('Stock', ['name', 'shares', 'price', 'date', 'time'])
+stock_prototype = Stock('', 0, 0.0, None, None)
+
+def dict_to_stock(s):
+    return stock_prototype._replace(**s)
+
+a = {'name': 'ACME', 'shares': 100, 'price': 123.45}
+print(dict_to_stock(a))
+
+b = {'name': 'ACME', 'shares': 100, 'price': 123.45, 'date': '12/17/2012'}
+print(dict_to_stock(b))
